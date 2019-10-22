71c973c8313ed3cc31f0f58bf7d9d5005ab0fd62
toyooka.yuto@baroque-w.jp <toyooka.yuto@baroque-w.jp>
2019-10-22 16:36:38 +0900

    #テスト作成

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,14 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
